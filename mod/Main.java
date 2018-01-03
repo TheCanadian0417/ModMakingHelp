3f89511dcc6441a6940f839b709fbc878ea0cbe7
TheCanadian0417 <31222417+TheCanadian0417@users.noreply.github.com>
2018-01-03 11:14:21 -0500

    Add files via upload

---
+++ mod/Main.java
@@ -0,0 +1,28 @@
+package hunter.mod;
+
+import hunter.mod.proxy.CommonProxy;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import util.Reference;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class Main 
+{
+	@Instance
+	public static Main instance;
+	
+	@SidedProxy(clientSide = Reference.CLIENT, serverSide = Reference.COMMON)
+	public static CommonProxy proxy;
+	
+	@EventHandler
+	public static void preInit(FMLPreInitializationEvent event) {}
+	@EventHandler
+	public static void init(FMLInitializationEvent event) {}
+	@EventHandler
+	public static void postinit(FMLPostInitializationEvent event) {}
+}
---
+++ mod/proxy/ClientProxy.java
@@ -0,0 +1,5 @@
+package hunter.mod.proxy;
+
+public class ClientProxy extends CommonProxy{
+
+}
