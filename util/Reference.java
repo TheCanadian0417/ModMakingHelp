3d058992151be66416d778b63972b4833e2d28fe
TheCanadian0417 <31222417+TheCanadian0417@users.noreply.github.com>
2018-01-03 11:16:59 -0500

    Add files via upload

---
+++ util/Reference.java
@@ -0,0 +1,11 @@
+package util;
+
+public class Reference 
+{
+	public static final String MODID = "cm";
+	public static final String NAME = "Canada Mod";
+	public static final String VERSION = "0.1 - MC 1.12.2";
+	
+	public static final String CLIENT= "";
+	public static final String COMMON = "";
+}
